2aa2e0cdde749e03360b8e427efae8b7fa8644e2
David Liu <chihweiliu1993@gmail.com>
2021-11-09 23:34:44 +0000

    basic placement

--- views/homepage/TimelineSection.tsx
+++ views/homepage/TimelineSection.tsx
@@ -4,6 +4,8 @@ import PageSection from '../../components/PageSection'
 import { timelineContents } from '../../data/timeline/timelineContents'
 
 const YEAR_HEIGHT = 500
+const LABEL_HEIGHT = 60
+
 const TimelineSection = () => {
   const timelineItems = Object.values(timelineContents).sort((a, b) => {
     if (isAfter(a.startDate, b.startDate)) {
@@ -16,46 +18,64 @@ const TimelineSection = () => {
 
   const years = differenceInYears(new Date(), ealiestDate)
 
-  const timelinePoints: number[] = []
+  const timelinePoints: number[] = [getYear(new Date())]
 
   for (let i = 0; i < years; i++) {
-    if (i === 0) {
-      timelinePoints.unshift(getYear(ealiestDate))
-    } else {
-      timelinePoints.unshift(timelinePoints[0] + 1)
-    }
+    timelinePoints.push(timelinePoints[i] - 1)
   }
 
   return (
     <PageSection title="Work Experience and Education">
-      {timelinePoints.map((year, index) => {
-        return (
-          <Flex key={year} flexDir="column" alignItems="center">
-            <Text fontWeight="semibold" fontSize="xl" mb="3px">
-              {year}
-            </Text>
+      <Box position="relative">
+        {timelinePoints.map((year, index) => {
+          return (
+            <Flex key={year} flexDir="column" alignItems="center" height={YEAR_HEIGHT + 'px'}>
+              <Flex flexDir="column" alignItems="center" height={LABEL_HEIGHT + 'px'}>
+                <Text fontWeight="semibold" fontSize="xl" mb="3px">
+                  {year}
+                </Text>
 
-            <Flex
-              borderRadius="100%"
-              borderColor="teal.500"
-              borderWidth="2px"
-              width="20px"
-              height="20px"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Box bgColor="teal.500" width="10px" height="10px" borderRadius="100%" />
+                <Flex
+                  borderRadius="100%"
+                  borderColor="teal.500"
+                  borderWidth="2px"
+                  width="20px"
+                  height="20px"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Box bgColor="teal.500" width="10px" height="10px" borderRadius="100%" />
+                </Flex>
+              </Flex>
+
+              {index < years && (
+                <Box
+                  bgColor="teal.500"
+                  height={YEAR_HEIGHT - LABEL_HEIGHT + 'px'}
+                  width="3px"
+                  mt="10px"
+                  mb="10px"
+                  borderRadius="5px"
+                />
+              )}
             </Flex>
+          )
+        })}
+        {timelineItems.map((item, index) => {
+          const startYear = getYear(item.startDate)
+          const endYear = getYear(item.endDate)
 
-            {index < years - 1 && (
-              <Box bgColor="teal.500" height={YEAR_HEIGHT + 'px'} width="3px" mt="10px" mb="10px" borderRadius="5px" />
-            )}
-          </Flex>
-        )
-      })}
-      {/* {timelineItems.map((item, index) => {
-        return <Box key={item.organisation + index}>{item.organisation}</Box>
-      })} */}
+          const numYears = differenceInYears(item.endDate, item.startDate)
+          const span = numYears * YEAR_HEIGHT
+          const top = (getYear(new Date()) - startYear) * YEAR_HEIGHT + LABEL_HEIGHT / 2
+
+          return (
+            <Box key={item.organisation + index} position="absolute" top={top + 'px'}>
+              {item.organisation}
+            </Box>
+          )
+        })}
+      </Box>
     </PageSection>
   )
 }
