59066f9f0b249a5645e0dcd8ff2c7c888e609273
sourceful-david <76947056+sourceful-david@users.noreply.github.com>
2021-12-18 18:13:10 +0100

    fix type

--- frontend/views/homepage/ContactSection.tsx
+++ frontend/views/homepage/ContactSection.tsx
@@ -1,6 +1,7 @@
 import { Grid, Text } from '@chakra-ui/react';
 import PageSection from '@components/PageSection';
 import SocialLink from '@components/SocialLink';
+import { SocialNetwork } from '@groq/fragments/AboutSection.fragment';
 
 const ContactSection = () => {
   return (
@@ -9,7 +10,7 @@ const ContactSection = () => {
         return (
           <Grid textAlign="center" justifyItems="center" gridGap={4} margin="90px 0px">
             <Text>Link my work? Contact me on Linkedin</Text>
-            <SocialLink socialNetwork="linkedin" />
+            <SocialLink socialNetwork={SocialNetwork.LINKEDIN} />
           </Grid>
         );
       }}
